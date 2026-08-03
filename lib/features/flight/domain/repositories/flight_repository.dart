600f23f10b219159fbdf6fcc6a78527426cdb922
Sajani-Prabhashika <sajaniprabhashika2024@gmail.com>
2026-08-03 13:39:19 +0530

    api connecting

--- lib/features/flight/domain/repositories/flight_repository.dart
+++ lib/features/flight/domain/repositories/flight_repository.dart
@@ -1,7 +1,9 @@
 import '../../domain/entities/flight.dart';
+import '../../domain/entities/flight.dart';
 import '../../domain/entities/flight_search_criteria.dart';
 
 abstract class FlightRepository {
   Future<List<Flight>> searchFlights(FlightSearchCriteria criteria);
+  Future<List<Flight>> getAllFlights();
   Future<Flight?> getFlightById(int id);
 }
