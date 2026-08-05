81deef260aff6c6e4c39a5b09410c665702744fb
Sajani-Prabhashika <sajaniprabhashika2024@gmail.com>
2026-08-05 18:15:24 +0530

    card pay method2

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -18,6 +18,10 @@ import '../../features/flight/data/datasources/flight_remote_datasource_impl.dar
 import '../../features/flight/data/repositories/flight_repository_impl.dart';
 import '../../features/flight/domain/repositories/flight_repository.dart';
 import '../../features/flight/presentation/bloc/flight_bloc.dart';
+import '../../features/payment/data/datasources/payment_remote_datasource.dart';
+import '../../features/payment/data/datasources/payment_remote_datasource_impl.dart';
+import '../../features/payment/data/repositories/payment_repository_impl.dart';
+import '../../features/payment/domain/repositories/payment_repository.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -66,5 +70,13 @@ Future<void> setupDependencyInjection() async {
   getIt.registerSingleton<BookingSession>(BookingSession());
   getIt.registerFactory<BookingBloc>(
     () => BookingBloc(getIt<BookingRepository>()),
+  );
+
+  // Payment
+  getIt.registerLazySingleton<PaymentRemoteDatasource>(
+    () => PaymentRemoteDatasourceImpl(getIt<ApiClient>()),
+  );
+  getIt.registerLazySingleton<PaymentRepository>(
+    () => PaymentRepositoryImpl(getIt<PaymentRemoteDatasource>()),
   );
 }
