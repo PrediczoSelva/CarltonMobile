5b9466424b635056c2efea1e8fd4893cf27e6532
Sajani-Prabhashika <sajaniprabhashika2024@gmail.com>
2026-08-05 18:23:14 +0530

    card pay 11

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'core/constants/app_constants.dart';
 import 'core/di/injection.dart';
+import 'core/network/api_client.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'features/auth/presentation/bloc/auth_bloc.dart';
@@ -12,6 +15,20 @@ Future<void> main() async {
 
   await Hive.initFlutter();
   await setupDependencyInjection();
+
+  try {
+    final apiClient = getIt<ApiClient>();
+    final response = await apiClient.get<dynamic>(AppConstants.paymentStripePublishableKey);
+    final key = response.data is Map
+        ? (response.data as Map)['publishableKey'] as String?
+        : null;
+    if (key != null && key.isNotEmpty) {
+      Stripe.publishableKey = key;
+      await Stripe.instance.applySettings();
+    }
+  } catch (_) {
+    // Stripe will remain uninitialised; card payments will show an error until configured.
+  }
 
   // TODO once Firebase project is created:
   // await Firebase.initializeApp();
