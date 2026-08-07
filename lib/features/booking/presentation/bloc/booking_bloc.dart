ef495d77f6b4af812403b15ce381b05ecbd7856b
Sajani-Prabhashika <sajaniprabhashika2024@gmail.com>
2026-08-08 01:09:20 +0530

    card pay 13

--- lib/features/booking/presentation/bloc/booking_bloc.dart
+++ lib/features/booking/presentation/bloc/booking_bloc.dart
@@ -7,6 +7,9 @@ import 'booking_state.dart';
 class BookingBloc extends Bloc<BookingEvent, BookingState> {
   BookingBloc(this._repository) : super(BookingInitial()) {
     on<CreateBookingRequested>(_onCreate);
+    on<CreateAtlasBookingRequested>(_onCreateAtlas);
+    on<CreateAmadeusBookingRequested>(_onCreateAmadeus);
+    on<CreateTravelportBookingRequested>(_onCreateTravelport);
     on<GetUserBookingsRequested>(_onGetUserBookings);
     on<CancelBookingRequested>(_onCancelBooking);
   }
@@ -26,6 +29,80 @@ class BookingBloc extends Bloc<BookingEvent, BookingState> {
         contactPhone: event.contactPhone,
         paymentMethod: event.paymentMethod,
         paymentMetadataJson: event.paymentMetadataJson,
+      );
+      emit(BookingSuccess(booking));
+    } catch (e) {
+      emit(BookingError(e.toString()));
+    }
+  }
+
+  Future<void> _onCreateAtlas(
+    CreateAtlasBookingRequested event,
+    Emitter<BookingState> emit,
+  ) async {
+    emit(BookingLoading());
+    try {
+      final booking = await _repository.createAtlasBooking(
+        routingIdentifier: event.routingIdentifier,
+        passengers: event.passengers,
+        contactName: event.contactName,
+        contactEmail: event.contactEmail,
+        contactPhone: event.contactPhone,
+        bookingClass: event.bookingClass,
+        quotedTotal: event.quotedTotal,
+        flightSnapshotJson: event.flightSnapshotJson,
+        stripePaymentIntentId: event.stripePaymentIntentId,
+        isGuest: event.isGuest,
+      );
+      emit(BookingSuccess(booking));
+    } catch (e) {
+      emit(BookingError(e.toString()));
+    }
+  }
+
+  Future<void> _onCreateAmadeus(
+    CreateAmadeusBookingRequested event,
+    Emitter<BookingState> emit,
+  ) async {
+    emit(BookingLoading());
+    try {
+      final booking = await _repository.createAmadeusBooking(
+        amadeusOfferToken: event.amadeusOfferToken,
+        verifyId: event.verifyId,
+        stripePaymentIntentId: event.stripePaymentIntentId,
+        baseFareTotal: event.baseFareTotal,
+        passengers: event.passengers,
+        contactEmail: event.contactEmail,
+        contactPhone: event.contactPhone,
+        bookingClass: event.bookingClass,
+        quotedTotal: event.quotedTotal,
+        flightSnapshotJson: event.flightSnapshotJson,
+        isGuest: event.isGuest,
+      );
+      emit(BookingSuccess(booking));
+    } catch (e) {
+      emit(BookingError(e.toString()));
+    }
+  }
+
+  Future<void> _onCreateTravelport(
+    CreateTravelportBookingRequested event,
+    Emitter<BookingState> emit,
+  ) async {
+    emit(BookingLoading());
+    try {
+      final booking = await _repository.createTravelportBooking(
+        fareKey: event.fareKey,
+        segmentKeys: event.segmentKeys,
+        passengers: event.passengers,
+        contactEmail: event.contactEmail,
+        contactPhone: event.contactPhone,
+        bookingClass: event.bookingClass,
+        quotedTotal: event.quotedTotal,
+        providerBaseFare: event.providerBaseFare,
+        flightSnapshotJson: event.flightSnapshotJson,
+        stripePaymentIntentId: event.stripePaymentIntentId,
+        isGuest: event.isGuest,
       );
       emit(BookingSuccess(booking));
     } catch (e) {
