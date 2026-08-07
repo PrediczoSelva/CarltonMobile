a637c024e0cd51f77570dd12e660552a547215cd
Sajani-Prabhashika <sajaniprabhashika2024@gmail.com>
2026-08-08 00:53:19 +0530

    card pay-2

--- android/app/src/main/kotlin/com/example/carlton_leisure_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/carlton_leisure_app/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.carlton_leisure_app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
