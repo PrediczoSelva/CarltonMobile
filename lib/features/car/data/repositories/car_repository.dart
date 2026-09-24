d1e9372d812b87810c8896c830844c96b38edd06
Sajani-Prabhashika <sajaniprabhashika2024@gmail.com>
2026-09-25 01:47:57 +0530

    car screen updates

--- lib/features/car/data/repositories/car_repository.dart
+++ lib/features/car/data/repositories/car_repository.dart
@@ -1,6 +1,8 @@
+import '../../domain/entities/car_extra_option.dart';
 import '../../domain/entities/car_search_criteria.dart';
 import '../../domain/entities/car_vehicle.dart';
 
 abstract class CarRepository {
   Future<List<CarVehicle>> searchCars(CarSearchCriteria criteria);
+  Future<List<CarExtraOption>> getExtraOptions(String vehicleId);
 }
